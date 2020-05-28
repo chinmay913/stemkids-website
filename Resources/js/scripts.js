de6d9279987237fab720c44e51d7efe4d663e16c
Chinmay Girolkar <chinmaygirolkar@gmail.com>
2020-05-28 22:44:53 +0530

    add animation, change logo

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -9,22 +9,130 @@ $(document).ready(function () {
       }
     },
     {
-      offset: "60px;",
+      offset: "60px",
     }
   );
 
-  $(".section-offer").waypoint(
+  /* Scroll on buttons */
+  $(".js--scroll-to-courses").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-courses").offset().top,
+      },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-offer").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-offer").offset().top,
+      },
+      1000
+    );
+  });
+
+  //   nav about option
+  $(".js--nav-about").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-offer").offset().top,
+      },
+      1000
+    );
+  });
+
+  //   nav courses option
+  $(".js--nav-courses").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-courses").offset().top,
+      },
+      1000
+    );
+  });
+
+  //   nav contact option
+  $(".js--nav-contact").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-contact").offset().top,
+      },
+      1000
+    );
+  });
+
+  //   nav home option
+  $(".js--nav-home").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".section-header").offset().top,
+      },
+      1000
+    );
+  });
+
+  /* Animations on scroll */
+  $(".js--wp-1").waypoint(
     function (direction) {
       if (direction == "down") {
-        $(".section-offer.row").addClass("animate__animated animate__fadeInUp");
+        $(".js--wp-1").addClass("animated fadeInUp");
       } else {
-        $(".section-offer.row").removeClass(
-          "animate__animated animate__fadeInUp"
-        );
+        $(".js--wp-1").removeClass("animated fadeInUp");
       }
     },
     {
-      offset: "60px;",
+      offset: "80%",
+    }
+  );
+
+  $(".js--wp-code-img").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".js--wp-code-img").addClass("animated fadeInLeft");
+      } else {
+        $(".js--wp-code-img").removeClass("animated fadeInLeft");
+      }
+    },
+    {
+      offset: "80%",
+    }
+  );
+  $(".js--wp-code-text").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".js--wp-code-text").addClass("animated fadeInRight");
+      } else {
+        $(".js--wp-code-text").removeClass("animated fadeInRight");
+      }
+    },
+    {
+      offset: "80%",
+    }
+  );
+
+  $(".js--wp-robo-img").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".js--wp-robo-img").addClass("animated fadeInRight");
+      } else {
+        $(".js--wp-robo-img").removeClass("animated fadeInRight");
+      }
+    },
+    {
+      offset: "80%",
+    }
+  );
+  $(".js--wp-robo-text").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".js--wp-robo-text").addClass("animated fadeInLeft");
+      } else {
+        $(".js--wp-robo-text").removeClass("animated fadeInLeft");
+      }
+    },
+    {
+      offset: "80%",
     }
   );
 });
